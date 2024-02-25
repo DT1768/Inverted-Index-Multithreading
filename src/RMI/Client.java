f5f1da9e345085a578e16d7ca533e5fe1bbad2a0
Dhruv Thakkar <dhruvthakkar1768@gmail.com>
2024-02-25 17:49:07 -0500

    Top 5 token implementation done

--- src/RMI/Client.java
+++ src/RMI/Client.java
@@ -2,8 +2,11 @@ package RMI;
 
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Client {
     public static void main(String[] args) {
@@ -18,7 +21,16 @@ public class Client {
             Map<String, List<Integer>> index = service.getInvertedIndex("test.txt");
 
             // Process and display the results
-            index.forEach((key, value) -> System.out.println(key + " found on lines: " + value));
+            List<Map.Entry<String, List<Integer>>> sortedEntries = index.entrySet().stream()
+                    .sorted(Collections.reverseOrder(Map.Entry.comparingByValue(Comparator.comparingInt(List::size))))
+                    .limit(5)
+                    .collect(Collectors.toList());
+
+            // Process and display the results
+            System.out.println("Top 5 most frequent words and their locations:");
+            for (Map.Entry<String, List<Integer>> entry : sortedEntries) {
+                System.out.println(entry.getKey() + " found on lines: " + entry.getValue());
+            }
         } catch (Exception e) {
             System.err.println("Client exception: " + e.toString());
             e.printStackTrace();
